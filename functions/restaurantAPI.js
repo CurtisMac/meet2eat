f09dc47f3e97d38b07440c2aa9d4ac30f08b10ce
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2018-03-01 13:56:05 -0800

    api data

---
+++ functions/restaurantAPI.js
@@ -0,0 +1,11 @@
+const axios = require('axios')
+
+axios({
+    method: 'get',
+    url: 'https://developers.zomato.com/api/v2.1/geocode?lat=49.285049&lon=-123.114723',
+    headers: { 'user-key': 'e8bb510b6bf9e4d7930830a4c9c3ba5e' }
+
+})
+    .then(function (response) {
+        console.log(response.data.nearby_restaurants)
+    });

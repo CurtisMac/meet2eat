51ca89e98d3b9390796cc7d9aaff55bd0aa2226b
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2018-03-02 10:18:15 -0800

    updates

--- routes/joinMeet.js
+++ routes/joinMeet.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const joinRouter = express.Router()
 
-joinRouter.get('/join', (req, res) => {
-    res.send('join a meetup')
-})
+// joinRouter.
 
-// app.get('/join', (req, res) => {
-//     let { username } = req.params
-//     console.log(username)
-//     //will receive picked time and picked food
-//     res.send(username)
+// joinRouter.post('/join', (req, res) => {
+//     // let {id}
+//     res.send('join a meetup')
 // })
+
+// // app.get('/join', (req, res) => {
+// //     let { username } = req.params
+// //     console.log(username)
+// //     //will receive picked time and picked food
+// //     res.send(username)
+// // })
 
 module.exports = joinRouter

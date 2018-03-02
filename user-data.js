a24cd4ad9ce6d8df230bbb197bae4c0b76f45f10
ed-silva <32685663+ed-silva@users.noreply.github.com>
2018-03-01 19:41:57 -0800

    data update

--- user-data.js
+++ user-data.js
@@ -1,11 +1,91 @@
 const faker = require('faker');
 
-const userDb = []
+const userDb = [
+    {
+        username: 'sandra',
+        password: 'abc123',
+        info: {
+            photo: "",
+            id: 0001,
+            name: 'Sandra',
+            age: '',
+            sex: 'female',
+            interests: [
+                {}
+            ],
+            bio: faker.lorem.paragraph(),
+            currentLocation: {
+                lat: 49.2850486,
+                long: -123.1169118,
+            }
+        }
+    },
+    {
+        username: 'eduardo',
+        password: 'abc123',
+        info: {
+            photo: "",
+            id: 0002,
+            name: 'Ed Silva',
+            age: '',
+            sex: 'male',
+            interests: [
+                {}
+            ],
+            bio: faker.lorem.paragraph(),
+            currentLocation: {
+                lat: 49.2850486,
+                long: -123.1169118,
+            }
+        }
+    },
+    {
+        username: 'curtis',
+        password: 'abc123',
+        info: {
+            photo: "",
+            id: 0003,
+            name: 'Curtis Mac',
+            age: '',
+            sex: 'male',
+            interests: [
+                {}
+            ],
+            bio: faker.lorem.paragraph(),
+            currentLocation: {
+                lat: 49.2850486,
+                long: -123.1169118,
+            }
+        }
+    },
+    {
+        username: 'andrew',
+        password: 'abc123',
+        info: {
+            photo: "",
+            id: 0004,
+            name: 'Andrew Sordier',
+            age: '',
+            sex: 'male',
+            interests: [
+                {}
+            ],
+            bio: faker.lorem.paragraph(),
+            currentLocation: {
+                lat: 49.2850486,
+                long: -123.1169118,
+            }
+        }
+    }
+]
+
 
 sex = () => {
     let x = faker.random.boolean()
-    x === true ? "female" : "male"
+    return x === true ? "female" : "male";
+
 }
+
 age = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -14,27 +94,25 @@ age = (min, max) => {
 
 for (let i = 0; i < 3; i++) {
     userDb.push({
-        
-            username: faker.internet.userName(),
-            password: faker.internet.password(),
-            info: {
-                photo: faker.image.avatar(),
-                id: faker.random.number(),
-                name: faker.name.findName(),
-                age: age(18, 65),
-                sex: sex(),
-                interests: [
-                    {}
-                ],
-                bio: faker.lorem.paragraph(),
-                currentLocation: {
-                    lat: 0,
-                    long: 0,
-                }
+        username: faker.internet.userName(),
+        password: faker.internet.password(),
+        info: {
+            photo: faker.image.avatar(),
+            id: faker.random.number(),
+            name: faker.name.findName(),
+            age: age(18, 65),
+            sex: sex(),
+            interests: [
+                {}
+            ],
+            bio: faker.lorem.paragraph(),
+            currentLocation: {
+                lat: 0,
+                long: 0,
             }
         }
-    );
+    });
 }
 
 console.log(userDb);
-console.log(userDb[0].info.name)
+// console.log(userDb[0].info.sex)
